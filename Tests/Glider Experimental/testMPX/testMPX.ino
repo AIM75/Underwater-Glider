e34f9a5bfbb405d1f806da362c03d653f0353957
Abdelrahman Ayman <43994297+IAbdelrahmanI@users.noreply.github.com>
2025-06-09 17:24:37 +0300

    minor detial in testMPX.ino

--- Tests/Glider Experimental/testMPX/testMPX.ino
+++ Tests/Glider Experimental/testMPX/testMPX.ino
@@ -1,44 +1,41 @@
 #include "DepthModule.h"
 
+#define MPX_PIN 33
+
 bool isAtSurface = false;
 
 // Module instances
-MPX5010 depthSensor(13);
+MPX5010 depthSensor(MPX_PIN);
 String command = "0";
 
 
 void setup() {
-    Serial.begin(115200);
-
-
+  Serial.begin(115200);
 }
 
 
 void loop() {
-    // Update sensor readings
-    updateSensorData();
+  // Update sensor readings
+  updateSensorData();
 
-    
-    delay(100); // Main loop delay
+
+  delay(100);  // Main loop delay
 }
 
 void updateSensorData() {
-    float depth = depthSensor.readDepthCm() / 100.0f; // meters
+  float depth = depthSensor.readDepthCm() / 100.0f;  // meters
 
-        
-    // Create data string
-    String data = String(millis()) + "," +
-                  String(depth, 2);
-    Serial.print("analogRead:  ");
-    Serial.println(analogRead(13));
-    Serial.print("Depth:  ");              
-    Serial.println(depth);
-    Serial.print("kPa:  ");
-    Serial.println(depthSensor.readPressurekPa());
-    Serial.print("CmH2O:  ");
-    Serial.println(depthSensor.readPressureCmH2O());
-    Serial.println("-----------------------------------------------------------------");
-    delay(400);
+
+  // Create data string
+  String data = String(millis()) + "," + String(depth, 2);
+  Serial.print("analogRead:  ");
+  Serial.println(analogRead(MPX_PIN));
+  Serial.print("Depth:  ");
+  Serial.println(depth);
+  Serial.print("kPa:  ");
+  Serial.println(depthSensor.readPressurekPa());
+  Serial.print("CmH2O:  ");
+  Serial.println(depthSensor.readPressureCmH2O());
+  Serial.println("-----------------------------------------------------------------");
+  delay(400);
 }
-
-
